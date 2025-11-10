62070c2e4078f9c14ec00acfde0b271447c9c3b4
MarcoMambrettiUdesa <mmambretti@udesa.edu.ar>
2025-11-10 14:46:51 -0300

    arranque con comentar un post

--- PIgrupo6/App.js
+++ PIgrupo6/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Login from './src/screens/Login';
 import Register from "./src/screens/Register";
 import HomeMenu from "./src/components/HomeMenu";
+import Comentar from "./src/screens/Comentar";
 
 
 
@@ -15,6 +16,8 @@ export default function App() {
         <Stack.Screen name='Login' component={ Login } options = { {headerShown: false} } />
         <Stack.Screen name='Register' component={ Register } options = { {headerShown: false} }/>
         <Stack.Screen name='HomeMenu' component={ HomeMenu } options = { {headerShown: false} }/>
+        <Stack.Screen name='Comentar' component={ Comentar } options = { {headerShown: false} }/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
